63973ed923137870812f3375d5aefe065349d517
arthur <arthur.wangc@dadaabc.com>
2018-10-22 21:38:19 +0800

    docs: update

--- src/demo-neon/rust/hello_struct/README.md
+++ src/demo-neon/rust/hello_struct/README.md
@@ -2,3 +2,9 @@
 
 - 定义和实例化结构
 - 当变量和字段具有相同名称时使用Field Init速记
+
+## Rust Struct Question
+> Rust 自动引用和解引用执行过程是怎样的？
+- 在编写 `impl` 时，&self 不存在引用时，称为？ 关联函数
+- 关联函数的用途？ 常用于创建一个特定的Sturct实体
+- 一个Struct可以有几个 `imple` ? 任意多个
--- src/demo-neon/rust/hello_struct/src/main.rs
+++ src/demo-neon/rust/hello_struct/src/main.rs
@@ -5,6 +5,13 @@ struct User {
     time_stamp: u32
 }
 
+impl User {
+    // add code here
+    fn hello(&self) {
+        println!("hello , my name is {}", self.username);
+    }
+}
+
 fn main() {
     // 每个属性都需要初始化
     let mut user1 = User {
@@ -13,10 +20,9 @@ fn main() {
         active: true,
         time_stamp: 32
     };
-    println!("Hello, world!, {}", user1.email);
-
+    user1.hello();
     let user2 = build_user("arthur".to_string(), "arthur@we.com".to_string(), true, 232323);
-    println!("hello, my friend {}", user2.email);
+    user2.hello();
 }
 
 fn build_user(username: String, email: String, active: bool, time_stamp: u32) -> User {
