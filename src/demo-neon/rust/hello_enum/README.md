628e959098dc0f8b989eacd4a0637525e6b20651
arthur <arthur.wangc@dadaabc.com>
2018-10-23 22:19:44 +0800

    feat: rust demo update

--- src/demo-neon/rust/hello_enum/README.md
+++ src/demo-neon/rust/hello_enum/README.md
@@ -5,7 +5,10 @@
 ## Question Rust Enum
 
 - 在Rust中不存在空值这一概念，那个使用什么类型代替呢？ Option
-- let t: Option<i8> = Option::some(3) 能否直接与 let s: i8 =2 相加呢？ 不能
+- `let t: Option <i8> = Option::some(3)` 能否直接与 `let s: i8 =2` 相加呢？ 不能
+- 结构体和枚举，例举几个相似点？ 1. 都能使用impl
+  
+## Rust match 控制流运算符
 
 ## 课后作业
 - 阅读[rust option文档](https://doc.rust-lang.org/std/option/enum.Option.html)
--- src/demo-neon/rust/hello_enum/src/main.rs
+++ src/demo-neon/rust/hello_enum/src/main.rs
@@ -1,7 +1,27 @@
-enum IpKind {
-    V4,
-    V6
+// 将任意类型的数据放入枚举成员中, 包括结构体
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String)
 }
+
+enum Message {
+    Quit,
+    // 包含一个匿名结构体
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    // 此处描述的不甚明了，需要查阅其他经典的实现方式
+     fn call(&self){
+         // 不知作何处理
+     }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let home = IpAddr::V4(127, 0, 0,1);
+    let lookback = IpAddr::V6(String::from("::1"));
+    let m = Message::Write(String::from("hello"));
+    let msg = m.call();
 }
