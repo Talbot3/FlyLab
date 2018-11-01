c85f07b98b5ef924032b44f52c3c9969b4e7a13a
arthur <arthur.wangc@dadaabc.com>
2018-11-01 09:40:53 +0800

    feat: trait default behave

--- src/demo-neon/rust/hello_trait/src/main.rs
+++ src/demo-neon/rust/hello_trait/src/main.rs
@@ -8,6 +8,38 @@ impl<T> Point<T> {
         &self.x
     }
 }
+// 定义指定类型的 trait
+pub trait Summarizable {
+    fn summary(&self) -> String;
+}
+
+// 定义默认行为
+pub trait SummarizableDefault {
+    fn summary(&self) -> String {
+        format!("default impl")
+    }
+}
+
+struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String
+}
+
+struct WeatherForecast {
+   hight_temp : f64,
+   low_temp: f64,
+   chance_of_precipitation:f64
+}
+impl SummarizableDefault for WeatherForeacast {}
+
+impl Summarizable for NewsArticle {
+    fn summary(&self) -> String {
+        format!("one statment news: {}, {}", self.headline, self.location)
+    }
+}
+
 
 fn main() {
     let integer = Point {
@@ -15,4 +47,12 @@ fn main() {
         y: 33.3,
     };
     println!("Hello, world! {:?} {:?}", integer.x(), integer.y);
+
+    let news = NewsArticle {
+        headline: String::from("早上的一天"),
+        location: String::from("上海长宁区"),
+        author: "arthur.wang".to_string(),
+        content: "eat breakfast".to_string()
+    };
+    println!("{}", news.summary());
 }
