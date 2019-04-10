e34686594141db53410115d0ab51d3c8f2184909
wangcheng <wangcheng@deepblueai.com>
2019-04-10 18:24:01 +0800

    feat: add libuv env

---
+++ src/demo-c_language/libuv/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+libuv_flags=`pkg-config --cflags --libs libuv`;
+
+if [$libuv_flags]; then
+  echo "libuv uninstall ..., please run `brew install libuv` on macos, other os query by google by you self"
+else 
+  echo "libuv installed ."
+  echo ${libuv_flags}
+fi
+
+clang -o uvtest main.c ${libuv_flags}
+./uvtest && rm -rf uvtest
