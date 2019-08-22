dd2ed58f323c0f8505f2c06cb518edce20ab8009
orangebook <1796560392@qq.com>
2019-08-22 18:36:09 +0800

    feat: play gstream - funny so much

---
+++ src/demo-gstream/README.md
@@ -0,0 +1,17 @@
+# GStream
+
+> want you play funny
+
+## 开发环境安装
+
+> 以MacOS为例
+
+```bash
+brew install gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-libav;
+# [ISSUE `Package 'libffi', required by 'gobject-2.0', not found`](https://github.com/otrv4/pidgin-otrng/issues/104)
+export PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig;
+```
+
+## 参考
+
+- [gstream tutorial](https://gstreamer.freedesktop.org/documentation/tutorials/basic/dynamic-pipelines.html?gi-language=c)
---
+++ src/demo-gstream/build.sh
@@ -0,0 +1,8 @@
+gcc basic-tutorial-1.c -o basic-tutorial-1 `pkg-config --cflags --libs gstreamer-1.0`
+gcc basic-tutorial-2.c -o basic-tutorial-2 `pkg-config --cflags --libs gstreamer-1.0`
+gcc basic-tutorial-3.c -o basic-tutorial-3 `pkg-config --cflags --libs gstreamer-1.0`
+gcc basic-tutorial-4.c -o basic-tutorial-4 `pkg-config --cflags --libs gstreamer-1.0`
+gcc basic-tutorial-5.c -o basic-tutorial-5 `pkg-config --cflags --libs gstreamer-video-1.0 gtk+-3.0 gstreamer-1.0`
+gcc basic-tutorial-6.c -o basic-tutorial-6 `pkg-config --cflags --libs gstreamer-1.0`
+gcc basic-tutorial-7.c -o basic-tutorial-7 `pkg-config --cflags --libs gstreamer-1.0`
+
