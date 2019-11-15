ec05d0543e90014ce1e850f00c39390d013bf7e0
Talbot3 <1796560392@qq.com>
2019-11-15 09:45:45 +0800

    feat: python bind

--- example/demo-cpython-bind/test.c
+++ example/demo-cpython-bind/test.c
@@ -7,10 +7,27 @@ static PyObject* helloworld(PyObject* self, PyObject* args)
     return Py_None;
 }
 
+static PyObject *method_fputs(PyObject *self, PyObject *args) {
+    char *str, *filename = NULL;
+    int bytes_copied = -1;
+
+    /* Parse arguments */
+    if(!PyArg_ParseTuple(args, "ss", &str, &filename)) {
+        return NULL;
+    }
+
+    FILE *fp = fopen(filename, "w");
+    bytes_copied = fputs(str, fp);
+    fclose(fp);
+
+    return PyLong_FromLong(bytes_copied);
+}
+
 // Our Module's Function Definition struct
 // We require this `NULL` to signal the end of our method
 // definition
 static PyMethodDef myMethods[] = {
+    {"fputs", method_fputs, METH_VARARGS, "Python interface for fputs C library function"},
     { "helloworld", helloworld, METH_NOARGS, "Prints Hello World" },
     { NULL, NULL, 0, NULL }
 };
@@ -24,8 +41,20 @@ static struct PyModuleDef myModule = {
     myMethods
 };
 
+
 // Initializes our module using our above struct
-PyMODINIT_FUNC PyInit_myModule(void)
-{
-    return PyModule_Create(&myModule);
+PyMODINIT_FUNC PyInit_myModule(void) {
+    /* Assign module value */
+    PyObject *module = PyModule_Create(&myModule);
+
+    /* Add int constant by name */
+    PyModule_AddIntConstant(module, "FPUTS_FLAG", 64);
+
+    /* Define int macro */
+    #define FPUTS_MACRO 256
+
+    /* Add macro to module */
+    PyModule_AddIntMacro(module, FPUTS_MACRO);
+
+    return module;
 }
---
+++ example/demo-cpython-bind/test_mymodule.py
@@ -0,0 +1,11 @@
+import myModule
+myModule.helloworld()
+
+def test_copy_data():
+    content_to_copy = "Real Python!"
+    bytes_copied = myModule.fputs(content_to_copy, 'test_write.txt')
+
+    with open('test_write.txt', 'r') as f:
+        content_copied = f.read()
+
+    assert content_copied == content_to_copy
