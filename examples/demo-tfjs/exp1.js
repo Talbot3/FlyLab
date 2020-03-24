d6e9eb8adc691b0033e4a50dc2e807b4a57a2129
Talbot3 <1796560392@qq.com>
2020-03-24 10:22:52 +0800

    feat: init tensor

--- examples/demo-tfjs/exp1.js
+++ examples/demo-tfjs/exp1.js
@@ -1,6 +1,9 @@
 const tf = require('@tensorflow/tfjs-node');
+const shape = [2,2];
+// 初始化疑是
+const t1 = tf.tensor([1,2,3,4], shape, 'int32');
 
-const shape = [2,3];
-const a = tf.tensor([1,2,3,10,20,30], shape);
-
-a.print();
+// 修改张量的形状
+const t2 = t1.reshape([4,1]);
+t1.print();
+t2.print();
