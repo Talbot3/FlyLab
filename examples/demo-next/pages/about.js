3b369bad2048979be3de7590ff51dd5dda4796ef
Talbot3 <1796560392@qq.com>
2020-04-09 18:13:37 +0800

    feat: next.js React.cloneElemnet

--- examples/demo-next/pages/about.js
+++ examples/demo-next/pages/about.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default class Home extends React.Component {
-  render() {
-    return <div>About me</div>;
-  }
+export default class Test extends Component{
+	render(){
+		return (
+			<Parent>
+				<Child />
+			</Parent>
+		)
+	}
+}
+
+class Parent extends Component {
+	constructor(){
+		super();
+		this.state = {
+			count: 1
+		};
+	}
+	getChildren(){
+		const _this = this;
+		let { children } = _this.props;
+		return React.Children.map(children, child => {
+			return React.cloneElement(child, {
+				count: _this.state.count
+			});
+		});
+	}
+	handleClick= ()=> {
+    // console.log(this.state.count);
+		this.setState({
+			count: ++this.state.count
+		});
+	}
+	render(){
+		return (
+			<div>
+				<button onClick={ this.handleClick.bind(this) }>点击增加次数</button>
+				{ this.getChildren() }
+			</div>
+		)
+	}
+}
+class Child extends Component{
+	render(){
+		return (
+			<div>
+				这是子组件：{ this.props.count }
+			</div>
+		)
+	}
 }
