5ef80d46497cb4be9a17a7a5ae7fbde30fbcf268
Talbot3 <1796560392@qq.com>
2020-08-25 11:48:59 +0800

    refactor:  pull/push

--- examples/demo-zeromq/example/pull_push/producer.js
+++ examples/demo-zeromq/example/pull_push/producer.js
@@ -3,7 +3,7 @@ const zmq = require("zeromq")
 async function run() {
   const sock = new zmq.Push
 
-  sock.connect("tcp://127.0.0.1:3000")
+  await sock.bind("tcp://127.0.0.1:3000")
   console.log("Producer bound to port 3000")
 
   while (true) {
--- examples/demo-zeromq/example/pull_push/worker.js
+++ examples/demo-zeromq/example/pull_push/worker.js
@@ -3,7 +3,7 @@ const zmq = require("zeromq")
 async function run() {
   const sock = new zmq.Pull
 
-  await sock.bind("tcp://127.0.0.1:3000")
+  sock.connect("tcp://127.0.0.1:3000")
   console.log("Worker connected to port 3000")
 
   for await (const [msg] of sock) {
