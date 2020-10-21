04eebecec641afbd9223c5e38c8bfa9161a26eec
Talbot3 <1796560392@qq.com>
2020-10-21 16:34:27 +0800

    feat: toJson

--- examples/demo-excel/index.js
+++ examples/demo-excel/index.js
@@ -1,4 +1,4 @@
-const ExcelJS = require('xlsx');
+const XLSX = require('xlsx');
 const path = require('path');
 const { readFile } = require('fs').promises;
 
@@ -10,49 +10,14 @@ const { readFile } = require('fs').promises;
     const excelBuffer = await readFile(excelPath);
 
     // 解析数据
-    const result = ExcelJS.read(excelBuffer, {
+    const workbook = XLSX.read(excelBuffer, {
         type: 'buffer',
         cellHTML: false,
     });
 
-    console.log(result);
-
-    // let excelPath = path.join(__dirname, "myExcelFile.xlsx").toString();
-
-    // let excelPath = path.join(__dirname, "./上海董家渡融创壹号院项目视频监控汇总.xls").toString();
-
-    // const workbook = new ExcelJS.stream.xlsx.WorkbookReader(excelPath);
-    // for await (const { eventType, value } of workbook.parse()) {
-    //     switch (eventType) {
-    //         case 'shared-strings':
-    //         // value is the shared string
-    //         case 'worksheet':
-    //         // value is the worksheetReader
-    //         case 'hyperlinks':
-    //         // value is the hyperlinksReader
-    //     }
-    // }
-    // const workbook = new ExcelJS.Workbook();
-    // workbook.calcProperties.fullCalcOnLoad = true;
-    // workbook.views = [
-    //     {
-    //         x: 0, y: 0, width: 10000, height: 20000,
-    //         firstSheet: 0, activeTab: 1, visibility: 'visible'
-    //     }
-    // ]
-    // await workbook.xlsx.readFile(path.join(__dirname, "./myExcelFile.xlsx").toString());
-
-    // console.log(excelPath);
-    // await workbook.xlsx.readFile(path.join(__dirname, "./上海董家渡融创壹号院项目视频监控汇总.xls").toString());
-    
-    // workbook.eachSheet((worksheet, sheetId) => {
-    //     console.log(worksheet, sheetId);
-    // });
-
-    // const cameraWorksheet = workbook.getWorksheet('摄像头信息');
-    // console.log(cameraWorksheet);
-
-    // const id1Worksheet = workbook.getWorksheet(1);
-    // console.log(id1Worksheet);
+    // console.log(workbook.SheetNames);
+    const sheet = workbook.Sheets['摄像头信息'];
+    // console.log(sheet);
+    console.log(XLSX.utils.sheet_to_json(sheet));
 
 })()
