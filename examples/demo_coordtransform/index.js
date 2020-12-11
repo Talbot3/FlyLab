4afc32cb140192dcfb1a968a850fd4d7ba25056f
Talbot3 <1796560392@qq.com>
2020-12-11 12:21:19 +0800

    feat: up

--- examples/demo_coordtransform/index.js
+++ examples/demo_coordtransform/index.js
@@ -3,6 +3,15 @@ const fs = require('fs');
 const path = require('path');
 const geoArr = require('./geo.json');
 
+function convertGCJ02(json) {
+    let [lon, lat] = coordtransform.wgs84togcj02(json.longitude, json.latitude);
+    return {
+        ...json,
+        longitude: lon,
+        latitude: lat
+    };
+}
+
 function convertBD09(json) {
     console.log('wgs84 坐标 (longitude, latitude) => (', json.longitude, ',', json.latitude, ')');
     let [lon, lat] = coordtransform.wgs84togcj02(json.longitude, json.latitude);
@@ -16,6 +25,18 @@ function convertBD09(json) {
     };
 }
 
-let result = geoArr.map((item)=> convertBD09(item));
-fs.writeFileSync(path.join(__dirname, '12-09-351440071281324.txt').toString(), JSON.stringify(result));
+let arrWGS84 = geoArr.map((item) => [item.longitude, item.latitude]);
+fs.writeFileSync(path.join(__dirname, './data/12-09-351440071281324-WGS84.json').toString(), JSON.stringify(arrWGS84));
 
+let resultGCJ02 = geoArr.map(item => convertGCJ02(item));
+let arrGCJ02 = resultGCJ02.map((item) => [item.longitude, item.latitude]);
+fs.writeFileSync(path.join(__dirname, './data/12-09-351440071281324-GCJ02.json').toString(), JSON.stringify(arrGCJ02));
+let pathGCJ02 = resultGCJ02.map((item, index) => ({ x: item.longitude, y: item.latitude, tm: index, sp: 10, ag: 10 }));
+fs.writeFileSync(path.join(__dirname, './data/12-09-351440071281324-PATH-GCJ02.json').toString(), JSON.stringify(pathGCJ02));
+
+let resultBD09 = geoArr.map((item) => convertBD09(item));
+fs.writeFileSync(path.join(__dirname, './data/12-09-351440071281324-BD09.txt').toString(), JSON.stringify(resultBD09));
+
+
+let resultArr = resultBD09.map((item) => [item.longitude, item.latitude]);
+fs.writeFileSync(path.join(__dirname, './data/12-09-351440071281324-BD09.json').toString(), JSON.stringify(resultArr));
