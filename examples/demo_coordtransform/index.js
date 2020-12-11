d081b0a99c4273596de8ebba94992001b4c0e529
Talbot3 <1796560392@qq.com>
2020-12-11 11:25:09 +0800

    feat: up

--- examples/demo_coordtransform/index.js
+++ examples/demo_coordtransform/index.js
@@ -1,11 +1,21 @@
 const coordtransform = require('coordtransform');
-const json = {
-    "longitude": "114.054353",
-    "latitude": "22.733980"
+const fs = require('fs');
+const path = require('path');
+const geoArr = require('./geo.json');
+
+function convertBD09(json) {
+    console.log('wgs84 坐标 (longitude, latitude) => (', json.longitude, ',', json.latitude, ')');
+    let [lon, lat] = coordtransform.wgs84togcj02(json.longitude, json.latitude);
+    console.log('gcj02坐标 (longitude, latitude) => (', lon, ',', lat, ')');
+    let [targetLon, targetLat] = coordtransform.gcj02towgs84(lon, lat);
+    console.log('bd09坐标 (longitude, latitude) => (', targetLon, ',', targetLat, ')');
+    return {
+        ...json,
+        longitude: targetLon,
+        latitude: targetLat
+    };
 }
 
-console.log('wgs84 坐标 (longitude, latitude) => (', json.longitude, ',', json.latitude, ')');
-let [lon, lat] = coordtransform.wgs84togcj02(json.longitude, json.latitude);
-console.log('gcj02坐标 (longitude, latitude) => (', lon, ',', lat, ')');
-let [targetLon, targetLat] = coordtransform.gcj02towgs84(lon, lat);
-console.log('bd09坐标 (longitude, latitude) => (',  targetLon, ',',targetLat, ')');
+let result = geoArr.map((item)=> convertBD09(item));
+fs.writeFileSync(path.join(__dirname, '12-09-351440071281324.txt').toString(), JSON.stringify(result));
+
